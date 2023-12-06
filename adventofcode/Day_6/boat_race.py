1f8e454b3ccf4f4e676e4ee46f8fb138bd189783
Okkers <Oskarmoesmand@gmail.com>
2023-12-06 22:07:23 +0100

    Day 6

---
+++ adventofcode/Day_6/boat_race.py
@@ -0,0 +1,23 @@
+with open("input.txt") as f:
+    data = f.read().split(":")
+    plc= data[1].split("\n")
+    times = [ x for x in plc[:len(plc)-1][0].split(" ") if x != ""]
+    d_plc = data[2].split("\n")
+    dists = [x for x in d_plc[:len(d_plc)-1][0].split(" ") if x != ""]
+
+winnings = []
+for indx in range(len(dists)):
+    num_win = 0
+    secs = int(times[indx])
+    dist = int(dists[indx])
+    for hold_button_down in range(secs):
+        dist_reached = hold_button_down * (secs-hold_button_down)
+        if dist_reached > dist:
+            num_win += 1
+    winnings.append(num_win)
+
+f = lambda acc, x: acc *x 
+accu = 1
+[accu := f(accu, x) for x in winnings]
+
+print("Answer for Puzzle 11:", accu)
---
+++ adventofcode/Day_6/boat_race_2.py
@@ -0,0 +1,20 @@
+with open("input.txt") as f:
+    data = f.read().split(":")
+    plc= data[1].split("\n")
+    time = int("".join([ x for x in plc[:len(plc)-1][0].split(" ") if x != ""]))
+    d_plc = data[2].split("\n")
+    dist = int("".join([x for x in d_plc[:len(d_plc)-1][0].split(" ") if x != ""]))
+
+ind = 0 
+found = False 
+ans = 0
+while not(found):
+    dist_reached = ind*(time-ind)
+    if dist_reached > dist:
+        ans = time-ind-ind+1 
+        print(ind)
+        found = True 
+    else:
+        ind += 1
+
+print("Answer for Puzzle 12:", ans)
