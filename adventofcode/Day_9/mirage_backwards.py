3efa12a22819388f3af7d290df3893a86d812b90
Okkers <Oskarmoesmand@gmail.com>
2023-12-18 19:13:26 +0100

    Day 9

---
+++ adventofcode/Day_9/mirage_backwards.py
@@ -0,0 +1,34 @@
+with open("input.txt") as f:
+    data = f.read().split("\n")[:-1]
+    data = [[int(z) for z in x.split(" ")] for x in data]
+
+def get_diffs(lst):
+    diff_dict = {}
+    diffs = []
+    end = False
+    cnt = 0
+    while not end:
+        for i in range(len(lst)-1):
+            diffs.append(lst[i+1]-lst[i])
+        if diffs.count(0) == len(diffs):
+            diffs.append(0)
+            diff_dict[cnt] = diffs
+            end = True 
+        else:
+            diff_dict[cnt] = diffs
+            lst = diffs 
+            diffs = []
+            cnt += 1
+
+    return diff_dict
+
+last_vals = []
+for ind,oasis in enumerate(data):
+    diff_dict = get_diffs(oasis)
+    for ind,key in enumerate(list(diff_dict.keys())[::-1]):
+        if key == 0:
+            last_vals.insert(0, oasis[0] - diff_dict[key][0])
+        else:
+            diff_dict[key-1].insert(0, diff_dict[key-1][0] - diff_dict[key][0])
+
+print("Solution to Day 9 - part 2:", sum(last_vals))
